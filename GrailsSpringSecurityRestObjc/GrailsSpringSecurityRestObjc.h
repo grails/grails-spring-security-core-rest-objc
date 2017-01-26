250a20b18beedbdb0167bbeeb017aaf959287a4f
sdelamo <sergio.delamo@softamo.com>
2017-01-26 07:49:12 +0100

    Initial Commit

---
+++ GrailsSpringSecurityRestObjc/GrailsSpringSecurityRestObjc.h
@@ -0,0 +1,19 @@
+//
+//  GrailsSpringSecurityRestObjc.h
+//  GrailsSpringSecurityRestObjc
+//
+//  Created by Sergio del Amo on 26/01/2017.
+//  Copyright © 2017 Grails. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GrailsSpringSecurityRestObjc.
+FOUNDATION_EXPORT double GrailsSpringSecurityRestObjcVersionNumber;
+
+//! Project version string for GrailsSpringSecurityRestObjc.
+FOUNDATION_EXPORT const unsigned char GrailsSpringSecurityRestObjcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GrailsSpringSecurityRestObjc/PublicHeader.h>
+
+
